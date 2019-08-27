241f0cf23f9d04e90c28e35960dfbba75f42780b
AxelBankuna <axe.618@gmail.com>
2019-08-27 16:05:10 +0200

    initial commit

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'johndoe@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Jane Doe',
+        email: 'janedoe@gmail.com',
+        status: 'inactive'
+    },
+    {
+        id: 3,
+        name: 'Peter Gabriel',
+        email: 'peterg@gmail.com',
+        status: 'active'
+    }
+];
+
+module.exports = members;
